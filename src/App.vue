fe5d1c22a486760a982d3209d5b4ca64a9b5b141
alinabzykova <bzykovaaa@gmail.com>
2026-04-22 17:18:44 +0300

    Задание 4 выполнено

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <nav class="nav">
       <RouterLink to="/">Лента</RouterLink>
       <RouterLink to="/stats">Статистика</RouterLink>
+      <router-link to="/profile">Профиль</router-link>
     </nav>
     <RouterView/>
   </div>
 </template>
+
